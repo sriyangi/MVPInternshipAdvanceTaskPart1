61d5781b06a607d7479448c7390186ba78392c04
Sriyangi <sriyangi@yahoo.com>
2023-11-14 16:02:05 +0530

    Created Project skeleton

---
+++ MarsQAAdvancePart1/Tests/ShareSkillsTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsQAAdvancePart1.Tests
+{
+    internal class ShareSkillsTest
+    {
+    }
+}
---
+++ MarsQAAdvancePart1/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MarsQAAdvancePart1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
